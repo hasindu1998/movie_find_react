fcf4b4ba9f2c7f0f0891775e9c18f4073fdcfcfb
Hasindu Chanuka <148543372+hasindu1998@users.noreply.github.com>
2025-07-01 00:14:41 +0530

    bug fixed

--- First-React-App/vite.config.js
+++ First-React-App/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/movie_find_react",
+  base: "/movie_find_react/",
 })
